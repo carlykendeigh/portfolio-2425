19925a355ca2574b1c6c62df56bbff9280d0dde6
carlykendeigh <carlykendeigh@gmail.com>
2024-09-16 14:16:57 -0500

    testing personalization

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Carly Kendeigh's Portfolio",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -74,9 +73,9 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "Carly Kendeigh",
         logo: {
-          alt: "My Site Logo",
+          alt: "Carly Kendeigh Logo",
           src: "img/logo.svg",
         },
         items: [
